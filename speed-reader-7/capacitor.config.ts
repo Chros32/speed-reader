dfb5a349076f2e124a67192d97948676bedd21e2
Chros32 <chris.s.barnes@live.co.uk>
2026-02-02 21:00:08 +0000

    Delete speed-reader-7 directory

--- speed-reader-7/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.speedreader.app',
-  appName: 'Speed Reader',
-  webDir: 'out'
-};
-
-export default config;
